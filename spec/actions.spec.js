58d8cdbda3ffec7fbe06014e0c4234e4d6aa648d
benjicwood <bencharlwood@gmail.com>
2017-05-14 23:36:43 +0100

    add more action tests for eventinfo actions

--- spec/actions.spec.js
+++ spec/actions.spec.js
@@ -40,4 +40,40 @@ describe('Actions', () => {
       expect(actions.fetchEventError.length).to.equal(1);
     });
   });
+  describe('actions.fetchEventRequest', () => {
+    it('has a FETCH_EVENT_INFO_REQUEST', () => {
+      expect(types.FETCH_EVENT_INFO_REQUEST).to.equal('FETCH_EVENT_INFO_REQUEST');
+    });
+    it('is a function', () => {
+      expect(actions.fetchEventInfoRequest).to.be.a('function');
+    });
+    it('returns an object', () => {
+      expect(actions.fetchEventInfoRequest()).to.be.an('object');
+    });
+  });
+  describe('actions.fetchEventInfoSuccess', () => {
+    it('has a FETCH_EVENT_INFO_SUCCESS', () => {
+      expect(types.FETCH_EVENT_INFO_SUCCESS).to.equal('FETCH_EVENT_INFO_SUCCESS');
+    });
+    it('is a function', () => {
+      expect(actions.fetchEventInfoSuccess).to.be.a('function');
+    });
+    it('should take 1 argument', function () {
+      expect(actions.fetchEventInfoSuccess.length).to.equal(1);
+    });
+  });
+  describe('actions.fetchEventError', () => {
+    it('has a FETCH_EVENT_INFO_ERROR', () => {
+      expect(types.FETCH_EVENT_INFO_ERROR).to.equal('FETCH_EVENT_INFO_ERROR');
+    });
+    it('is a function', () => {
+      expect(actions.fetchEventInfoError).to.be.a('function');
+    });
+    it('returns an object', () => {
+      expect(actions.fetchEventInfoError()).to.be.an('object');
+    });
+    it('should take 1 argument', function () {
+      expect(actions.fetchEventInfoError.length).to.equal(1);
+    });
+  });
 });
